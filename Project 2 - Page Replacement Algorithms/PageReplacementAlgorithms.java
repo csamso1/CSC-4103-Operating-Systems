2d6c710221bbb8cd83c30f01cb9b6781338e99c8
Clayton Samson <csamso1@lsu.edu>
2017-03-20 11:46:32 -0500

    Created PageReplacementAlgorithms.java project

---
+++ Project 2 - Page Replacement Algorithms/PageReplacementAlgorithms.java
@@ -0,0 +1,17 @@
+/*
+Name:		Samson, Clayton
+Project: 	PA-2 (Page Replacement Algorithms)
+File:		pageref.txt
+Instructor:	Feng Chen
+Class:		cs4103-sp17
+LoginID:	cs410385
+*/
+
+public class PageReplacementAlgorithms
+{
+	//MAIN Class
+    public static void main(String[] args) throws FileNotFoundException
+    {
+
+    }
+}
