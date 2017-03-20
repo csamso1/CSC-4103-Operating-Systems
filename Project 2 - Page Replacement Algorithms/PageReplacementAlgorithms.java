1b67d1aa59ae63547e46a3ce0c4d336c580fd279
Clayton Samson <csamso1@lsu.edu>
2017-03-20 17:02:54 -0500

    Added error checking for comand line arguments

--- Project 2 - Page Replacement Algorithms/PageReplacementAlgorithms.java
+++ Project 2 - Page Replacement Algorithms/PageReplacementAlgorithms.java
@@ -14,15 +14,19 @@ import java.util.Scanner;
 public class PageReplacementAlgorithms
 {
 	//MAIN Class
-    public static void main(String[] args) throws FileNotFoundException
+    public static void main(String[] args) throws FileNotFoundException, Exception
     {
         //Uses command line arguments to set algorithm, cache size, and the input file name
+        try{
+            String algorithm = args[0];
+            int cacheSize = Integer.parseInt(args[1]);
+            String inputFileName = args[2];
+        } catch(Exception e){
+            throw new Exception("ERROR: Invalid command line args.\nPlease provide Algorithm (String), Cache Size (Integer), Input File Name (String) as command line arguments.", e);
+        }
         String algorithm = args[0];
         int cacheSize = Integer.parseInt(args[1]);
         String inputFileName = args[2];
-
-
-        // System.out.printf("Using input file: %s", inputFileName);
     	Scanner scan = new Scanner(new File(inputFileName));
         PageFile pageQueue[] = new PageFile[550];
         int i = 0;
@@ -33,8 +37,10 @@ public class PageReplacementAlgorithms
             System.out.printf("page%d added to pageQueue :: %b pID: %d\n", i, pageQueue[i].getWriteStatus(), pageQueue[i].getPageID());
             i++;
         }
-        //Scanning page files completed
+        //END scanning page files
 
+
+        //Print statments for verification
         System.out.printf("System Cache Size: %d\n", cacheSize);
         System.out.printf("Algorithm to use: %s\n", algorithm);
 
