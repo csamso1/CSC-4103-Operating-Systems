646800b7d1e1988418b9c6db1cd882e716d760b6
Clayton Samson <csamso1@lsu.edu>
2017-04-25 17:00:51 -0500

    Current Update

--- Project 3 - Shell Programming/sysbar.sh
+++ Project 3 - Shell Programming/sysbar.sh
@@ -4,6 +4,7 @@ if [ "$1" = "help" ]; then
 #Checks number of arguments provided is equal to 2
 elif [ $# -ne 2 ]; then
 	echo "Invalid number of arguments, please provide 2 arguments <interval in seconds> and <number of collections>"
+	echo "Use ./sysbar.sh help to access help instructions."
 else
 	#Saves CMD line arguments to arg1 and arg2 vars
 	arg1=$1
@@ -12,3 +13,7 @@ else
 	echo "arg1 = " $arg1
 	echo "arg2 = " $arg2
 fi
+echo "iostat results: "
+iostat $arg1 $arg2
+echo "mpstat results: "
+mpstat $arg1 $arg2
