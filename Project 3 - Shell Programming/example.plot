672c0a6fbd61004f83532d8184fa11b56ec321f0
Clayton Samson <csamso1@lsu.edu>
2017-04-26 09:16:25 -0500

    Not quite working right yet

---
+++ Project 3 - Shell Programming/example.plot
@@ -0,0 +1,14 @@
+=stacked; A; B
+title=This is an illustration title
+colors=yellow,red
+xlabel=This is X Label
+ylabel=This is Y Label
+=table
+=norotate
+yformat=%g
+
+0:0:0		10	50
+0:1:0		20	60
+0:2:0		30	70
+0:3:0		40	80
+0:4:0		50	90
--- Project 3 - Shell Programming/sysbar.sh
+++ Project 3 - Shell Programming/sysbar.sh
@@ -10,10 +10,38 @@ else
 	arg1=$1
 	arg2=$2
 	#Print statment for confirming argument inputs
-	echo "arg1 = " $arg1
-	echo "arg2 = " $arg2
+	echo "arg1 =" $arg1
+	echo "arg2 =" $arg2
 fi
-echo "iostat results: "
-iostat $arg1 $arg2
-echo "mpstat results: "
-mpstat $arg1 $arg2
+#iostat $arg1 $arg2
+#mpstat $arg1 $arg2
+#Setting up disk_usage.plot header
+echo "=stacked; A; B" >> disk_usage.plot
+echo "title=Disk Utilization" >> disk_usage.plot
+echo "colors=yellow,red" >> disk_usage.plot
+echo "xlabel=Time of data collection in HH:MM:SS" >> disk_usage.plot
+echo "ylabel=Bandwith (Kb/sec)" >> disk_usage.plot
+echo "=table" >> disk_usage.plot
+echo "=norotate" >> disk_usage.plot
+echo "yformat=%g" >> disk_usage.plot
+#Setting up cpu_usage.plot header
+echo "=stacked; A; B" >> cpu_usage.plot
+echo "title=CPU Utilization" >> cpu_usage.plot
+echo "colors=yellow,red" >> cpu_usage.plot
+echo "xlabel=Time of data collection in HH:MM:SS" >> cpu_usage.plot
+echo "ylabel=Percentage (%)" >> cpu_usage.plot
+echo "=table" >> cpu_usage.plot
+echo "=norotate" >> cpu_usage.plot
+echo "yformat=%g" >> cpu_usage.plot
+#For loop to run test for defined interval
+for (( i=1; i<=$arg2; i++)); do
+	io_results="$(iostat)"
+	mp_results="$(mpstat)"
+	current_time="$(date +"%T")"
+	echo $current_time
+	#echo "$(date +"%T")" >> disk_usage.plot
+	#echo "$(date +"%T")" >> cpu_usage.plot
+	echo $current_time "${io_results}" | awk '{print $3;}' >> disk_usage.plot
+	echo $current_time "${mp_results}" | awk '{print $4;}' >> cpu_usage.plot
+	sleep $arg1
+done
